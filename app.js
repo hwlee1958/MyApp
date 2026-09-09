f40df37b709d621951e68671035b64f6a1489cb4
hwlee1958 <39392207+hwlee1958@users.noreply.github.com>
2026-09-09 12:41:02 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('clickBtn').addEventListener('click', () => {
+    const resultEl = document.getElementById('result');
+    resultEl.innerText = '📱 앱이 정상적으로 작동하고 있습니다!';
+});
